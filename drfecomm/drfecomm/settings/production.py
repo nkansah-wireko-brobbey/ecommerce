60d12bd91d29a3a9419d4e62a78225995e0be977
nkansah-wireko-brobbey <wirekobrobbeyofficial@gmail.com>
2025-01-11 20:11:53 -0600

    modularize settings

---
+++ drfecomm/drfecomm/settings/production.py
@@ -0,0 +1,3 @@
+from .base import *
+
+print(f"{DEBUG}")
--- drfecomm/manage.py
+++ drfecomm/manage.py
@@ -2,11 +2,17 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+from drfecomm.settings import base
 
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'drfecomm.settings')
+
+    if  base.DEBUG is True:
+        os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'drfecomm.settings.local')
+    else: 
+        os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'drfecomm.settings.production')
+
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
